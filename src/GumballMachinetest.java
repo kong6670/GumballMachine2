be3d072305f52d51766139b7be15e2f7fb690336
kong6670 <30824358+kong6670@users.noreply.github.com>
2017-10-17 11:57:44 +0700

    add method

--- src/GumballMachinetest.java
+++ src/GumballMachinetest.java
@@ -3,7 +3,15 @@ public class GumballMachinetest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		GumballMachine gumballMachine = new GumballMachine(3);
+			
+		System.out.println(gumballMachine);
+		gumballMachine.ejectQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		
+		System.out.println(gumballMachine);
+		
 	}
 
 }
